24181d046ba7c4240801d9aecd8f14e391d0bea2
Lucas Fischer <fischer-lucas@hotmail.com>
2022-08-24 19:19:32 +0100

    update

--- Curso_de_JavaScript/projetos/assets/js/scripts.js
+++ Curso_de_JavaScript/projetos/assets/js/scripts.js
@@ -2,15 +2,38 @@ var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    else {
+        stop
+    }
 }
 
 function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber > -10) {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    else {
+        stop
+    }
 }
 
 
 
 
+
+
+
+
+    
+
+
+
+
+/*const changecolor = document.getElementById("currentNumber");
+if (currentNumber < 0){
+	document.getElementById("currentNumber").style.color = "red";
+}*/
